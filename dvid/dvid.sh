2d739f368ff39407461b3713e4ba6918baff2592
William Grisaitis <grisaitisw@janelia.hhmi.org>
2017-02-15 15:42:08 -0500

    Update DVID Dockerfile for DVID @ 6b5bb3a, console v1.14.10

--- dvid/dvid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ${1} != "" ]; then
-    sed "s/ZZ/${1}/g" /var/configbucket.toml > /var/configtemp.toml
-    dvid serve /var/configtemp.toml
-else
-    dvid serve /var/config.toml
-fi 
-
